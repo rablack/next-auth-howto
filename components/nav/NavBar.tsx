1ee168e8e8d2b797a479286122c56602f5e0621b
Robert Black <rablack@users.noreply.github.com>
2024-11-11 12:29:12 -0800

    Fixed menu border color in light mode

--- components/nav/NavBar.tsx
+++ components/nav/NavBar.tsx
@@ -11,7 +11,7 @@ const links: NavLink[] = [{ name: "Home", href: "/" }];
 const NavBar = () => {
   return (
     <div className="flex justify-center">
-      <div className="flex p-4 border-white border-2 max-w-3xl flex-auto justify-between">
+      <div className="flex p-4 border-current border-2 max-w-3xl flex-auto justify-between">
         <div className="flex gap-4 flex-row flex-auto">
           {links.map((link) => {
             return (
